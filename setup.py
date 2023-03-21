577f56bed40c6b05f1f0e49433272b119155ec04
Glauco Lauria Marques Filho <glaucowow@hotmail.com.br>
2023-03-21 18:50:56 -0300

    preparing_env

--- setup.py
+++ setup.py
@@ -1,11 +1,9 @@
-def configuration(parent_package='', top_path=None):
-    from numpy.distutils.misc_util import Configuration
-    config = Configuration('typing', parent_package, top_path)
-    config.add_subpackage('tests')
-    config.add_data_dir('tests/data')
-    return config
+import numpy as np
+import pandas as pd
+from distutils.core import setup
+from Cython.Build import cythonize
 
+with open("README.md", "r") as f:
+    long_description = f.read()
 
-if __name__ == '__main__':
-    from numpy.distutils.core import setup
-    setup(configuration=configuration)
+setup(name="dead_band", ext_modules=cythonize("dead_band.pyx"), version="1.0.2")
