7c4ead9a750c5a23a0f55521737d649f5067902c
glauco.filho <glaucolmf@hotmail.com>
2025-04-12 14:53:38 -0300

    reset_repo

--- setup.py
+++
@@ -1,7 +0,0 @@
-from Cython.Build import cythonize
-from distutils.core import setup
-
-with open("README.md", "r") as f:
-    long_description = f.read()
-
-setup(name="dead_band", ext_modules=cythonize("dead_band.pyx"), version="1.0.3")
