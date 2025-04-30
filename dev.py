35a591ef27acb2770e5486a5877b6d35c2a13b72
glauco.filho <glaucolmf@hotmail.com>
2025-04-29 22:49:43 -0300

    generate_without_quality

--- dev.py
+++ dev.py
@@ -4,11 +4,9 @@ import datetime
 import time
 
 
-
-
 if __name__ == "__main__":
     start = datetime.datetime(2020, 1, 1, 0, 0, 0)
-    end = datetime.datetime(2025, 12, 31, 23, 59, 59, 999)
+    end = datetime.datetime(2020, 1, 31, 23, 59, 59, 999)
     seed = 42
 
     ts_i = time.time()
@@ -22,3 +20,17 @@ if __name__ == "__main__":
     ts_f = time.time()
     duration = ts_f - ts_i
     print(f"{duration:.4f} seconds to filter data")
+
+    ts_i = time.time()
+    data = generate_timeseries(seed, start, end, generate_quality=False)
+    ts_f = time.time()
+    duration = ts_f - ts_i
+    print(
+        f"{duration:.4f} seconds to generate the time series without quality"
+    )
+
+    ts_i = time.time()
+    filtered_data = slow_deadband(data, 10, 30)
+    ts_f = time.time()
+    duration = ts_f - ts_i
+    print(f"{duration:.4f} seconds to filter data without quality")
